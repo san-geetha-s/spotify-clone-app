b72a20afb913cdff1e2ff161cfaf5e146f2fe7d6
Sangeetha <sangeethas12122000@gmail.com>
2022-12-10 14:11:40 +0530

    user authentication is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-
+import React, {useEffect, useState} from 'react';
 import './App.css';
-
+import {getTokenFromUrl} from './Spotify'
+import Login from './Login';
 function App() {
+  const[token,setToken]=useState(null);
+ useEffect(()=>{
+
+   const hash=getTokenFromUrl()
+   window.location.hash=""
+    
+   const _token=hash.access_token;
+   if(_token){
+    setToken(_token)
+   }
+
+   console.log("I have a token ",token);
+ },[])
   return (
     <div className="App">
+      
+
+      {
+        token?
+        <h1>I am logged in</h1>
+        : 
+        <Login></Login>
+      }
       
     </div>
   );
